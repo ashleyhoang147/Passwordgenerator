6631cb71063cc81f05e21cdf71e0155ea04b9666
Ashley Hoang <ashleyhoang_147@yahoo.com>
2020-06-10 22:58:30 -0400

    fixed random number var copy function still not working

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
 // generate random password
 function generate(){
 //password length is random between 8 to 128
-min = 8
-max = 128
-let passlength = function getRandomArbitrary(min, max) 
-    {return Math.random() * (max - min) + min;}
+var min = 8;
+var max = 128;
+let passlength = Math. floor (Math.random() * (max - min) + min);
+
+//let passlength = 10;
 //available values for password inputs
 let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 let password =""
@@ -12,4 +13,13 @@ let password =""
 for(var i = 0; i <= passlength; i++){
     password = password + values.charAt(Math.floor(Math.random()*Math.floor(values.length - 1)));
 }
+//add the password generated to the text box
+document.getElementById("generatedpassword").value = password;
+
 }
+/*copy password to clipboard
+function copypassword(){
+    document.getElementById("generatedpassword").select();
+    document.execCommand("Copy");
+    alert("Your password is copied to clipboard");
+}*/
