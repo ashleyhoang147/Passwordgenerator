f7b8bc143ba6973a8b0dd21f86fc2405e0abcf53
Ashley Hoang <ashleyhoang_147@yahoo.com>
2020-06-10 21:42:23 -0400

    structuring functions to generate password

--- assets/script.js
+++ assets/script.js
@@ -3,5 +3,13 @@ function generate(){
 //password length is random between 8 to 128
 min = 8
 max = 128
-let passlenghth = function getRandomArbitrary(min, max) 
+let passlength = function getRandomArbitrary(min, max) 
     {return Math.random() * (max - min) + min;}
+//available values for password inputs
+let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+let password =""
+//loop for password generating
+for(var i = 0; i <= passlength; i++){
+    password = password + values.charAt(Math.floor(Math.random()*Math.floor(values.length - 1)));
+}
+}
